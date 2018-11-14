de9088da61f2b952d2b8b91e1a619c0f6a5d927c
Zekai Oguz Ozalp <zekai.ozalp@asyasoft.net>
2018-11-14 11:19:38 +0300

    Added webpack and babel.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("It works!");
-
+// import ffmpeg from 'ffmpeg-lambda-binary';
+//
+// const convertToMp4 = (source, destination) => ffmpeg(['-i', source, '-c', 'copy', '-y', destination])
+//     .then(() => object);
+//
+console.log("Starting...");
+// convertToMp4("input.mov", "output.mp4")
+//     .then(result => console.log("Completed.", result))
+//     .catch(error => console.log("Failed", error));
