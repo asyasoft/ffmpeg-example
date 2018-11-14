60a81e29b5d17f96aa08b640dd55466af10c321c
Zekai Oguz Ozalp <zekai.ozalp@asyasoft.net>
2018-11-14 14:07:02 +0300

    Updated webpack config.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// import ffmpeg from 'ffmpeg-lambda-binary';
-//
-// const convertToMp4 = (source, destination) => ffmpeg(['-i', source, '-c', 'copy', '-y', destination])
-//     .then(() => object);
-//
-console.log("Starting...");
-// convertToMp4("input.mov", "output.mp4")
-//     .then(result => console.log("Completed.", result))
-//     .catch(error => console.log("Failed", error));
+const path = require("path");
+const ffmpegBinaries = require('@ffmpeg-installer/ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
+
+console.log("ffmpeg binaries", ffmpegBinaries);
+ffmpeg.setFfmpegPath(ffmpegBinaries.path);
+ffmpeg(path.resolve('samples/input.mov'))
+    .format('mp4')
+    .save(path.resolve('samples/output.mp4'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
     mode: 'production',
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        libraryTarget: 'commonjs2'
     },
     module: {
         rules: [{
@@ -13,5 +15,19 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.FLUENTFFMPEG_COV': false
+        })
+    ],
+    externals: [
+        "@ffmpeg-installer/ffmpeg",
+        "fluent-ffmpeg"
+    ],
+    target: "node",
+    node: {
+        'child_process': "empty",
+        fs: "empty"
     }
 };
